24e553d7114facf25ac527ee1232fbed4ef56940
Remi THOMAS <remi@xtware.com>
2020-11-12 19:19:41 +0100

    First version

---
+++ DownloadTS/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblInfos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adresse m3u8";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtURL.Location = new System.Drawing.Point(24, 116);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(564, 29);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.Text = "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/gear3/prog_index.m3u8";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(24, 209);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(93, 39);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Démarrer";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(24, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(564, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(24, 174);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(564, 29);
+            this.txtName.TabIndex = 5;
+            this.txtName.Text = "Demo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 149);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nom de la vidéo";
+            // 
+            // lblInfos
+            // 
+            this.lblInfos.AutoSize = true;
+            this.lblInfos.Location = new System.Drawing.Point(53, 38);
+            this.lblInfos.Name = "lblInfos";
+            this.lblInfos.Size = new System.Drawing.Size(0, 21);
+            this.lblInfos.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 299);
+            this.Controls.Add(this.lblInfos);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Télécharger vidéo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblInfos;
+    }
+}
+
---
+++ DownloadTS/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Diagnostics.Process m_p = new System.Diagnostics.Process();
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists($"{txtName.Text}.mp4"))
+            {
+                MessageBox.Show($"Le fichier {txtName.Text}.mp4 existe déjà.\r\nVeuillez changer de nom ou l'effacer.");
+                return;
+            }
+            string[] items = txtURL.Text.Split('/');
+            System.Net.Http.HttpClient c = new System.Net.Http.HttpClient();
+            var r = await c.GetAsync(txtURL.Text);
+            if (r.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string s = await r.Content.ReadAsStringAsync();
+                r.Dispose();
+
+                string[] tmp = s.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                List<string> parts = new List<string>();
+                foreach (string s2 in tmp)
+                {
+                    if (!s2.StartsWith("#") && !parts.Contains(s2) && !string.IsNullOrEmpty(s2))
+                        parts.Add(s2);
+                }
+                progressBar1.Maximum = parts.Count;
+                string name = Guid.NewGuid().ToString();
+                using (StreamWriter sw = new StreamWriter(name))
+                {
+                    int count = 0;
+                    foreach (string s2 in parts)
+                    {
+                        progressBar1.Value = ++count;
+                        lblInfos.Text = $"Téléchargement partie {count}/{parts.Count}";
+                        Application.DoEvents();
+                        items[items.Length - 1] = s2;
+                        r = await c.GetAsync(string.Join("/", items));
+                        if (r.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            await r.Content.CopyToAsync(sw.BaseStream);
+                        }
+                    }
+                }
+                lblInfos.Text = $"Optimisation de la taille de la vidéo";
+                Application.DoEvents();
+
+                m_p.StartInfo.FileName = "ffmpeg.exe";
+                m_p.StartInfo.Arguments = $"-i \"{name}\" {txtName.Text}.mp4";
+                m_p.StartInfo.UseShellExecute = false;
+                m_p.EnableRaisingEvents = true;
+                m_p.Exited += delegate (object sender2, EventArgs e2)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblInfos.Text = "Terminé";
+                        System.IO.File.Delete(name);
+                    });
+                };
+                m_p.Start();
+            }
+        }
+    }
+}
